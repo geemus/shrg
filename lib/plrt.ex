f6695b696e8ff03258db3c5a4aa39a5d5ed4564a
geemus <geemus@gmail.com>
2014-11-11 23:26:13 -0600

    add route for links, setup basic fake functionality

--- lib/plrt.ex
+++ lib/plrt.ex
@@ -5,8 +5,14 @@ defmodule Plrt do
   plug :match
   plug :dispatch
 
+  get ":code" do
+    conn
+    |> put_resp_header("Location", "https://google.com#q=#{code}")
+    |> send_resp(307, "Temporary Redirect")
+  end
+
   match _ do
-    halt(send_resp(conn, 200, "Hello world"))
+    halt(send_resp(conn, 404, "Not Found"))
   end
 
 end
